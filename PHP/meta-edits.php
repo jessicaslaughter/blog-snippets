16438b6cf203ea08555fbbc5ce08bedc7d6f8cc0
Jessica <jessicaslaughter@utexas.edu>
2016-04-09 15:37:50 -0500

    added meta-edits.php

---
+++ PHP/meta-edits.php
@@ -0,0 +1,13 @@
+//* Customize the entry meta in the entry header
+add_filter( 'genesis_post_info', 'sp_post_info_filter' );
+function sp_post_info_filter($post_info) {
+	$post_info = '[post_date]';
+	return $post_info;
+}
+
+//* Customize the entry meta in the entry footer
+add_filter( 'genesis_post_meta', 'sp_post_meta_filter' );
+function sp_post_meta_filter($post_meta) {
+	$post_meta = '[post_categories]  [post_comments zero="Comments" one="Comments" more="Comments"]';
+	return $post_meta;
+}
